6cedf469cebbe3ac6f3408c43da9be4327641863
刘慧 <liuhui@liuhuideiMac.local>
2017-11-21 17:40:12 +0800

    初始化精简解码项目

---
+++ 05视频硬解码码H264/05视频硬解码码H264/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  05视频硬解码码H264
+//
+//  Created by 刘慧 on 2017/11/21.
+//  Copyright © 2017年 fusheng@myzhenzhen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 05视频硬解码码H264/05视频硬解码码H264/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  05视频硬解码码H264
+//
+//  Created by 刘慧 on 2017/11/21.
+//  Copyright © 2017年 fusheng@myzhenzhen.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
